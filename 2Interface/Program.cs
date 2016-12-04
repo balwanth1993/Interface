f65419cc6480a7868b3d7b46c8a07c4ec31da1e9
balwanth1993 <balwanth.r93@gmail.com>
2016-12-03 18:03:31 -0600

    Add project files.

---
+++ 2Interface/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Interface
+{
+    interface IEmployee
+    {
+        void EmployeeId(int id);
+        void EmployeeName(string emp);
+    }
+    abstract public class Employee : IEmployee
+    {
+        public abstract void EmployeeId(int id);
+
+        public void EmployeeName(string emp)
+        {
+            Console.WriteLine("Employee name is " + emp);
+        }
+
+    }
+    interface Icustomer
+    {
+        void DepositMoney();
+        void WithDrawMoney();
+    }
+
+    public class Banking : Employee, Icustomer
+    {
+        public void DepositMoney()
+        {
+            Console.WriteLine("Your Money has been Deposited");
+        }
+
+        public override void EmployeeId(int id)
+        {
+            Console.WriteLine("Your Employee Id is " +id);
+        }
+
+        public void WithDrawMoney()
+        {
+            Console.WriteLine("Your Money has been Withdrawn");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Banking bnk = new Banking();
+                bnk.EmployeeId(1);
+                bnk.EmployeeName("Balwanth");
+                bnk.DepositMoney();
+                bnk.WithDrawMoney();
+            }
+        }
+    }
+}
